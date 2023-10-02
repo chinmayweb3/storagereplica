b8b7a5e1ffb9a4d6852bd8821580d5c530b6d26d
chinmayweb3 <chinmayweb3@gmail.com>
2023-10-02 23:09:16 +0530

    test 1

--- src/app/api/healthcheck/route.ts
+++ src/app/api/healthcheck/route.ts
@@ -3,5 +3,5 @@ import { NextRequest, NextResponse } from "next/server";
 export const runtime = "edge";
 
 export async function GET(request: NextRequest) {
-  return new NextResponse("ok working");
+  return new NextResponse("ok workings");
 }
