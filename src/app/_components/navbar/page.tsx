f726ed1ade82d310333cfa6d7c6a8e0263451fa6
chinmayweb3 <chinmayweb3@gmail.com>
2023-12-04 16:18:17 +0530

    tailwind ts file master

--- src/app/_components/navbar/page.tsx
+++ src/app/_components/navbar/page.tsx
@@ -1,6 +1,4 @@
 "use client";
-import { Button } from "@/app/components/ui/button";
-import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
 import LoginBtn from "./login";
@@ -8,11 +6,9 @@ import LoginBtn from "./login";
 const Navbar = () => {
   return (
     <nav className="fixed top-0 left-0 w-full h-[56px] bg-background shadow-lg flex-center ">
-      <div className="center-Container flex justify-between items-center">
-        <Link href={"/"} className="font-roboto">
-          LOGO
-        </Link>
-        <div className="font-roboto ">
+      <div className="center-Container flex justify-between items-center font-roboto">
+        <Link href={"/"}>LOGO</Link>
+        <div>
           <LoginBtn />
         </div>
       </div>
